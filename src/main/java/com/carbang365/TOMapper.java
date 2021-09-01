a42c051c212efde8858ca242b85cbd235774dfc0
SHJ <SHJ@DESKTOP-QCLQACU>
2021-09-01 17:13:03 +0900

    주석변경

--- src/main/java/com/carbang365/TOMapper.java
+++ src/main/java/com/carbang365/TOMapper.java
@@ -419,7 +419,7 @@ public interface TOMapper {
 	public List<Map<String, Object>> selectBoardList(Map<String, Object> map);
 	// 게시판상태 Count
 	public List<Map<String, Object>> selectBoardStatusCnt(Map<String, Object> map);
-	// 게시판답변 Count+1
+	// 게시판답변 Count
 	public int updateReplyCnt(Map<String, Object> map);
 	// 게시판답변조회
 	public List<Map<String, Object>> selectBoardReply(Map<String, Object> map);
