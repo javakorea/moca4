c5811dc76534fcf01fa2e6331ba2fde53a44e44c
SHJ <SHJ@DESKTOP-QCLQACU>
2021-12-21 19:28:39 +0900

    sms 문자 깨짐 수정

--- src/main/java/com/carbang365/TOController.java
+++ src/main/java/com/carbang365/TOController.java
@@ -5258,13 +5258,13 @@ public class TOController{
 	    	
 	    	String BOARD_CONT = paramMap.get("GET_CONT_TXT").toString();
 	    	System.out.println("원본BOARD_CONT~~~~~~~~~~~>>>"+BOARD_CONT);
-	    	BOARD_CONT = BOARD_CONT.replaceAll("\\s", "");
+	    	BOARD_CONT = BOARD_CONT.replaceAll("(?ism)(\\<.*?\\>)","").replaceAll("(?ism)(\\&.*?\\;)","").replaceAll("(?ism)[^a-zA-Z0-9 ㄱ-ㅎㅏ-ㅣ가-힣]","");
 			System.out.println("변경BOARD_CONT~~~~~~~~~~~>>>"+BOARD_CONT);
 	    	List EFGPRPOP_list = mocaEFLService.selectList_EFGPROP(map);
 	    	if(EFGPRPOP_list != null && EFGPRPOP_list.size() > 0){
 	    		Map phonenumber = (Map)EFGPRPOP_list.get(0);
 		    	API.sendSms(
-		    			"[teammoca발신]\n"+"테스트<br>​​​​​​​1<br>".replaceAll("(?ism)(\\<.*?\\>)","").replaceAll("(?ism)[^a-zA-Z0-9 ㄱ-ㅎㅏ-ㅣ가-힣]",""),
+		    			"[teammoca발신]\n"+BOARD_CONT,
 		    			phonenumber.get("PROP_VALUE").toString(),
 		    			paramMap.get("BOARD_WRITER").toString()
 		    	);
