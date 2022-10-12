ec6eb7ccd3fc1f5623cae0d66d0e9d856e57030c
HP <thehack@naver.com>
2022-10-12 21:03:12 +0900

    수평선 ckeditor추가

--- WebContent/ckeditor/config.js
+++ WebContent/ckeditor/config.js
@@ -27,15 +27,15 @@ CKEDITOR.editorConfig = function( config ) {
 	*/
 	config.toolbar = [{
         name: 'document',
-        items: ['Source']
+        items: ['Maximize','Source','ExportPdf']
       },
       {
           name: 'basicstyles',
-          items: ['Bold']
+          items: ['Bold','Undo', 'Redo']
       },
       { 
       	name: 'styles', 
-      	items: ['FontSize'] 
+      	items: ['FontSize','Outdent','Indent','HorizontalRule']  
       },
       { 
     	name: 'colors',
@@ -72,7 +72,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.font_names	 = '굴림;궁서;돋움;바탕;Arial;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana' ; 
 	config.fullPage = false;   //true이면 html태그 까지 보임 false이면 에디터 부분 태그만 보임(html로 보기 시에)
 	config.enterMode = CKEDITOR.ENTER_BR; //엔터시 입력되는 태그 BR P DIV 세 종류가 있습니다.
-	config.fontSize_sizes = '8/8px;9/9px;10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;36/36px;48/48px;72/72px' ; //폰트 사이즈 설정입니다.
+	config.fontSize_sizes = '8/8px;9/9px;10/10px;11/11px;12/12px;14/14px;16/16px;18/18px;20/20px;22/22px;24/24px;26/26px;28/28px;' ; //폰트 사이즈 설정입니다.
 	config.startupFocus = false; //시작시 editor에 포커스 가는 여부 입니다. 마지막에 그린 에디터로 갑니다.
 	
 	//config.skin ="moono";
@@ -82,7 +82,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.pasteFromWordRemoveFontStyles = false;
 	config.pasteFromWordRemoveStyles = false;
 	
-	config.extraPlugins = 'pastefromexcel,pastefromword,resize,colordialog,tableresize,imageresizerowandcolumn,youtube'; 
+	config.extraPlugins = 'pastefromexcel,pastefromword,resize,colordialog,tableresize,imageresizerowandcolumn,youtube,horizontalrule'; 
 	config.resize_dir = 'vertical';// both || vertical || horizontal
 	config.toolbarStartupExpanded = false; // 툴바 접기
 	config.removePlugins = 'elementspath,resize';
